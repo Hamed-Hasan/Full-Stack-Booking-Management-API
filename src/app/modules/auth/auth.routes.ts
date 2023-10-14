cac60a0304a3b6aeb62777d61ca514cc0cbdf6fa
John Doe <hamedhasan422421@gmail.com>
2023-10-14 15:27:21 +0300

    chore: 🚀 Add registration route feat: ✨ Implement user login endpoint fix: 🐛 Fix token refreshing logic docs: 📝 Update password change route style: 💅 Refactor code for consistency test: 🧪 Add unit tests for authentication

---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AuthValidation } from './auth.validation';
+import { AuthController } from './auth.controller';
+
+const router = express.Router();
+
+// Route for User Registration
+router.post(
+  '/register',
+  validateRequest(AuthValidation.registerZodSchema),
+  AuthController.registerUser
+);
+
+// Route for User Login
+router.post(
+  '/login',
+  validateRequest(AuthValidation.loginZodSchema),
+  AuthController.loginUser
+);
+
+// Route for Refreshing Tokens
+router.post(
+  '/refresh-token',
+  AuthController.refreshToken
+);
+
+// Route for Changing Password
+router.post(
+  '/change-password',
+  validateRequest(AuthValidation.changePasswordZodSchema),
+  AuthController.changePassword
+);
+
+
+export const AuthRoutes = router;
