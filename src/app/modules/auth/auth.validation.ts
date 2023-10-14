8df7d7d1efccebdfee6ba91cacb3c4fc26517315
John Doe <hamedhasan422421@gmail.com>
2023-10-14 15:26:39 +0300

    Add 🚀 registerZodSchema validation Fix ✅ loginZodSchema password validation Update 🔒 changePasswordZodSchema validation Refactor 🔄 AuthValidation schemas Improve 🌟 email validation messages Enhance 📝 password validation errors

---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+const registerZodSchema = z.object({
+  body: z.object({
+    email: z.string().email({ message: 'Invalid email address' }),
+    password: z.string().min(6, { message: 'Password too short' })
+  }),
+});
+
+const loginZodSchema = z.object({
+  body: z.object({
+    email: z.string().email({ message: 'Invalid email address' }),
+    password: z.string().min(6, { message: 'Password too short' }),
+  }),
+});
+
+const changePasswordZodSchema = z.object({
+  body: z.object({
+    oldPassword: z.string().min(6, { message: 'Password too short' }),
+    newPassword: z.string().min(6, { message: 'Password too short' }),
+  }),
+});
+
+export const AuthValidation = {
+  registerZodSchema,
+  loginZodSchema,
+  changePasswordZodSchema,
+};
