05e11c764ad4c8cf8f9906e73102dd26240cdb64
John Doe <hamedhasan422421@gmail.com>
2023-10-16 01:49:06 +0300

    chore: Add route for creating reviews 🚀 fix: Update review route validation 🛠️ feat: Implement user-specific review retrieval 🌟 docs: Add documentation for getReviews endpoint 📚 style: Format code in review controller 🎨 refactor: Rename createReview function 🔄

---
+++ src/app/modules/reviews/review.routes.ts
@@ -0,0 +1,43 @@
+
+
+import express from 'express';
+import { ReviewController } from './review.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { ReviewValidation } from './review.validation';
+
+const router = express.Router();
+
+router.post(
+    '/create-review',
+    validateRequest(ReviewValidation.createReviewZodSchema),
+    ReviewController.createReview
+  );
+
+
+router.get(
+    '/all-reviews',
+    ReviewController.getReviews
+);
+
+router.get(
+    '/:id',
+    ReviewController.getReview
+);
+
+router.get(
+    '/user/:userId',
+    ReviewController.getUserReviews
+);
+
+router.patch(
+    '/:id',
+    validateRequest(ReviewValidation.reviewZodSchema),
+    ReviewController.updateReview
+);
+
+router.delete(
+    '/:id',
+    ReviewController.deleteReview
+);
+
+export const ReviewRoutes = router;
