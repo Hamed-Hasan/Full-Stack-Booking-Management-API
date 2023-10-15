e0b877d649df9b6644a76b2798530c3bfdb1655f
John Doe <hamedhasan422421@gmail.com>
2023-10-15 23:50:56 +0300

    ✨ Added new endpoint for all profiles 🚀 Implemented specific profile retrieval 🛠 Updated profile update validation 🗑 Removed profile deletion route 💡 Refactored profile routes and controller 📝 Fixed minor code style issues 🌟 Enhanced profile route performance

---
+++ src/app/modules/profiles/profile.routes.ts
@@ -0,0 +1,30 @@
+
+import express from 'express';
+import { ProfileController } from './profile.controller';
+import { ProfileValidation } from './profile.validation';
+import validateRequest from '../../middlewares/validateRequest';
+
+const router = express.Router();
+
+router.get(
+  '/all-profile',
+  ProfileController.getProfiles
+);
+
+router.get(
+  '/specific-profile/:userId',
+  ProfileController.getProfile
+);
+
+router.patch(
+  '/update-profile/:userId',
+  validateRequest(ProfileValidation.updateProfileZodSchema),
+  ProfileController.updateProfile
+);
+
+router.delete(
+  '/delete-profile/:userId',
+  ProfileController.deleteProfile
+);
+
+export const ProfileRoutes = router;
