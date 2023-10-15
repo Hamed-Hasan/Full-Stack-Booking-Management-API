461525df24d306f5b4b15744254e8c52926761a6
John Doe <hamedhasan422421@gmail.com>
2023-10-15 21:49:19 +0300

    🚀 Added new cart item functionality. 🔄 Updated cart item handling. 🗑️ Removed cart item handling. 📋 Listed cart items successfully. 🛒 Retrieved cart item from the cart.

---
+++ src/app/modules/cart/cart.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { CartItemService } from './cart.service';
+
+
+const addCartItem = catchAsync(async (req: Request, res: Response) => {
+  const result = await CartItemService.addCartItem(req.body);
+  sendResponse(res, { statusCode: 201, success: true, message: 'Item added to cart successfully.', data: result });
+});
+
+const updateCartItem = catchAsync(async (req: Request, res: Response) => {
+  const result = await CartItemService.updateCartItem(req.params.id, req.body);
+  sendResponse(res, { statusCode: 200, success: true, message: 'Cart item updated successfully.', data: result });
+});
+
+const removeCartItem = catchAsync(async (req: Request, res: Response) => {
+  await CartItemService.removeCartItem(req.params.id);
+  sendResponse(res, { statusCode: 200, success: true, message: 'Item removed from cart successfully.' });
+});
+
+const listCartItems = catchAsync(async (req: Request, res: Response) => {
+  const result = await CartItemService.listCartItems(req.params.userId);
+  sendResponse(res, { statusCode: 200, success: true, message: 'Cart items listed successfully.', data: result });
+});
+
+const getCartItem = catchAsync(async (req: Request, res: Response) => {
+    const cartItem = await CartItemService.getCartItem(req.params.id);
+    sendResponse(res, { statusCode: 200, success: true, message: 'Cart item retrieved successfully.', data: cartItem });
+  });
+
+export const CartItemController = {
+  addCartItem,
+  updateCartItem,
+  removeCartItem,
+  listCartItems,
+  getCartItem
+};
