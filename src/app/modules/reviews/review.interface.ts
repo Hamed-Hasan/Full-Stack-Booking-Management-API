b863422574f71729de4f8c25c0a4cb1a550a93ae
John Doe <hamedhasan422421@gmail.com>
2023-10-16 01:45:49 +0300

    🚀 Added the RatingValue enum for ratings. 📝 Implemented the IReview interface for user reviews. 🔧 Updated eslint rules for better code quality. 🐛 Fixed a bug in the review service. 🌟 Improved the rating system for services. 📦 Refactored the code for better organization.

---
+++ src/app/modules/reviews/review.interface.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+
+export enum RatingValue {
+    ONE = "ONE",
+    TWO = "TWO",
+    THREE = "THREE",
+    FOUR = "FOUR",
+    FIVE = "FIVE"
+  }
+  
+  export type IReview = {
+    id?: string;
+    userId: string;
+    serviceId: string;
+    review: string;
+    rating: RatingValue;
+  };
+  
