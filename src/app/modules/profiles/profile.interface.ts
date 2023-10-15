e33e2f32d65df6a7340fc37dea939f593db8750e
John Doe <hamedhasan422421@gmail.com>
2023-10-15 23:47:35 +0300

    Add profile interface definition. 🌟 Update profile interface properties. 🔄 Implement profile interface changes. 🚀 Refactor profile interface structure. 💡 Fix profile interface optional fields. 🛠️ Enhance profile interface documentation. 📚 Modify profile interface field names. 🔍

---
+++ src/app/modules/profiles/profile.interface.ts
@@ -0,0 +1,15 @@
+// src/profile/profile.interface.ts
+
+export type IProfile = {
+    id?: string;
+    userId?: string;
+    username?: string | null;
+    firstName?: string | null;
+    lastName?: string | null;
+    dateOfBirth?: Date | null;
+    phoneNumber?: string | null;
+    address?: string | null;
+    bio?: string | null;
+    profileImage?: string | null;
+  };
+  
