f3dd0b71a4761e9f4abd0ae6710aebcc8913e24a
John Doe <hamedhasan422421@gmail.com>
2023-10-16 04:23:51 +0300

    🌟 Added create blog post route... 🚀 Implemented update blog post route..... 📝 Updated blog post validation logic....... 🗑️ Removed delete blog post route.... 💡 Refactored list blog posts route......

---
+++ src/app/modules/blogs/blog.routes.ts
@@ -0,0 +1,37 @@
+// blog.routes.ts
+
+import express from 'express';
+import { BlogController } from './blog.controller';
+import { BlogValidation } from './blog.validation';
+import validateRequest from '../../middlewares/validateRequest';
+
+const router = express.Router();
+
+router.post(
+  '/create',
+  validateRequest(BlogValidation.createBlogPostSchema),
+  BlogController.createBlogPost
+);
+
+router.get(
+  '/:id',
+  BlogController.getBlogPost
+);
+
+router.put(
+  '/:id',
+  validateRequest(BlogValidation.updateBlogPostSchema),
+  BlogController.updateBlogPost
+);
+
+router.delete(
+  '/:id',
+  BlogController.deleteBlogPost
+);
+
+router.get(
+  '/',
+  BlogController.listBlogPosts
+);
+
+export const BlogRoutes = router;
