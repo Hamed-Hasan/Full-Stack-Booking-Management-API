44e972f3923422b1d98247da689348b3e3a9c930
John Doe <hamedhasan422421@gmail.com>
2023-10-16 15:19:39 +0300

    Add UserRole enum values 🌟 Update IUser interface 🚀 Fix eslint-disable comments 💡 Implement user authentication 🔒 Refactor admin role handling 🛠️ Add superAdmin role 🌐

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+export enum UserRole {
+    USER = 'user',
+    ADMIN = 'admin',
+    SUPER_ADMIN='superAdmin'
+}
+
+export type IUser = {
+    id?: string;
+    email: string;
+    password: string;
+    needsPasswordChange?: boolean;
+    role: UserRole;
+};
+
+
+
