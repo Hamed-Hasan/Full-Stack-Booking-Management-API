3f2bc474c4b915c80c4def0b01c79da5f670df93
John Doe <hamedhasan422421@gmail.com>
2023-10-16 03:51:59 +0300

    🚀 Implemented AvailabilityValidation.createUpdateAvailabilitySchema ✨ Added daysOfWeek schema to createUpdateAvailabilitySchema 🛠️ Refactored AvailabilityValidation module 📦 Updated import statements for zod 📝 Updated comments and documentation 🐛 Fixed a bug in the daysOfWeekSchema definition 💡 Improved code readability and structure

---
+++ src/app/modules/availability/availability.validation.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+const daysOfWeekSchema = z.object({
+  Monday: z.boolean().optional(),
+  Tuesday: z.boolean().optional(),
+  Wednesday: z.boolean().optional(),
+  Thursday: z.boolean().optional(),
+  Friday: z.boolean().optional(),
+  Saturday: z.boolean().optional(),
+  Sunday: z.boolean().optional(),
+});
+
+const createUpdateAvailabilitySchema = z.object({
+body: z.object({
+    serviceId: z.string(),
+    startDate: z.date(),
+    endDate: z.date(),
+    startTime: z.string(),
+    endTime: z.string(),
+    daysOfWeek: daysOfWeekSchema,
+})
+});
+
+export const AvailabilityValidation = {
+  createUpdateAvailabilitySchema,
+};
