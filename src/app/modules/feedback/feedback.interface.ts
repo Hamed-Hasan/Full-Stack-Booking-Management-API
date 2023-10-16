cbb2db3a214bed852a9f14eef1cc2cdbe04425e9
John Doe <hamedhasan422421@gmail.com>
2023-10-16 12:14:08 +0300

    Add create-feedback route 🚀 Update FeedbackRoutes 🔄 Implement deleteFeedback route 🗑️ Refactor code for better readability 📚 Fix validation bug in updateFeedback route 🐞 Create listFeedback route 📋

---
+++ src/app/modules/feedback/feedback.interface.ts
@@ -0,0 +1,8 @@
+export type IFeedback = {
+    id?: string;
+    userId: string;
+    content: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+  };
+  
---
+++ src/app/modules/feedback/feedback.routes.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import { FeedbackController } from './feedback.controller';
+import { FeedbackValidation } from './feedback.validation';
+import validateRequest from '../../middlewares/validateRequest';
+
+const router = express.Router();
+
+router.post(
+  '/create-feedback',
+  validateRequest(FeedbackValidation.createFeedbackZodSchema),
+  FeedbackController.createFeedback
+);
+
+router.get(
+  '/:id',
+  FeedbackController.getFeedback
+);
+
+router.put(
+  '/:id',
+  validateRequest(FeedbackValidation.updateFeedbackZodSchema),
+  FeedbackController.updateFeedback
+);
+
+router.delete(
+  '/:id',
+  FeedbackController.deleteFeedback
+);
+
+router.get(
+  '/',
+  FeedbackController.listFeedback
+);
+
+export const FeedbackRoutes = router;
